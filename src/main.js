687373ec4fd540df041139080cd3180a76940dec
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-01-11 08:32:24 -0800

    setup project environment.

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+$(document).ready(function() {
+  $('#').submit(function(event){
+
+  });
+});
